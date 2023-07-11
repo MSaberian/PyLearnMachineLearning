c885043e91446e89b1d2ecff7da4ed7ca2052b2d
MSaberian <mo.saberian@yahoo.com>
2023-07-11 18:58:59 +0330

    2

---
+++ PyLearnMLKNN2/knn.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+class KNN:
+    def __init__(self, k):
+        self.k = k
+    
+    def fit(self, X, Y):
+        self.X_train = X
+        self.Y_train = Y
+
+    def euclidean_distance(self, x1, x2):
+        return np.sqrt(np.sum((x1-x2)**2))
+
+    def predict(self, X):
+        results = []
+        for x in X:
+            distances = []
+            for x_train in self.X_train:
+                d = self.euclidean_distance(x_train, x)
+                distances.append(d)
+
+            nearest_neighbors = np.argsort(distances)[0:self.k]
+            result = np.bincount(self.Y_train[nearest_neighbors])
+            results.append([np.argmax(result), max(np.sort(distances)[0:self.k])])
+        return results
+
+    def evaluate(self, X, Y):
+        Y_pred = self.predict(X)
+        Y_pred = np.asarray(Y_pred)[:,0].astype(dtype='int')
+        accuracy = np.sum(Y_pred == Y) / len(Y)
+        return accuracy
